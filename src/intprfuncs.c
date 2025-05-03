5a10ec12b03c786636abb11b323f431f6d993bc6
Michael Jagiello <michaeljr623@hotmail.com>
2025-05-03 17:46:04 -0400

    added ,[] to interpreter, I/O test, small fixes

--- src/intprfuncs.c
+++ src/intprfuncs.c
@@ -21,7 +21,7 @@ int getfilename(int argc, char* argv[], char* filename) {
 
 int openfile(FILE** fptr, char* filename) {
     *fptr = fopen(filename, "r");
-    if (fptr == NULL) {
+    if (*fptr == NULL) {
         return FAIL;
     }
     return SUCCESS;
@@ -36,14 +36,24 @@ int interpret(FILE* fptr, char* data) {
     int dataIndex = 0;
 
     while ((ch = fgetc(fptr)) != EOF) {
+        index++;
+        int bracketCount = 0;
         switch (ch) {
             // increment data pointer
             case '>':
                 dataIndex++;
+                if (dataIndex >= DATA_SIZE) {
+                    printf("Out of data range, attempted access at %d.\n", dataIndex);
+                    return FAIL;
+                }
                 break;
             // decrement data pointer
             case '<':
                 dataIndex--;
+                if (dataIndex < 0) {
+                    printf("Out of data range, attempted access at %d.\n", dataIndex);
+                    return FAIL;
+                }
                 break;
             // increment byte at data pointer
             case '+':
@@ -59,31 +69,60 @@ int interpret(FILE* fptr, char* data) {
                 break;
             // input byte to data pointer
             case ',':
-
+                data[dataIndex] = getchar();
                 break;
             // if data == 0, jump forward to matching ]
             case '[':
-
+                if (data[dataIndex] != 0) {
+                    break;
+                }
+                while ((ch = fgetc(fptr))) {
+                    index++;
+                    if (ch == ']') {
+                        if (bracketCount == 0) {
+                            break;
+                        }
+                        bracketCount--;
+                    }
+                    else if (ch == '[') {
+                        bracketCount++;
+                    }
+                }
                 break;
             // if data != 0, jump backward to matching [
             case ']':
-
+                if (data[dataIndex] == 0) {
+                    break;
+                }
+                fseek(fptr, -2, SEEK_CUR);
+                index -= 2;
+                while ((ch = fgetc(fptr))) {
+                    index++;
+                    if (ch == '[') {
+                        if (bracketCount == 0) {
+                            break;
+                        }
+                        bracketCount--;
+                    }
+                    else if (ch == ']') {
+                        bracketCount++;
+                    }
+                    fseek(fptr, -2, SEEK_CUR);
+                    index -= 2;
+                    if (index < 0) {
+                        printf("Out of instruction range, attempted access at %d.\n", index);
+                        return FAIL;
+                    }
+                }
                 break;
             default:
                 break;
         }
-        index++;
         if (index < 0) {
             printf("Out of instruction range, attempted access at %d.\n", index);
             return FAIL;
         }
-        if (dataIndex < 0 || dataIndex >= DATA_SIZE) {
-            printf("Out of data range, attempted access at %d.\n", dataIndex);
-            return FAIL;
-        }
     }
-
-    //fseek(fptr, -2, SEEK_CUR);
 
     return SUCCESS;
 }
